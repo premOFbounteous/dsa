a6cc2044d74e0c5ea781a2f4559c1260ce20b71d
premOFbounteous <prem.maity@bounteous.com>
2025-11-24 16:32:56 +0530

    Add files via upload

---
+++ 1stOcurrenceBinary.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BS(vector<int> & arr,int s,int e,int num){
+    int start=s;
+    int end=e;
+    int mid;
+    int res=-1;
+
+    while(start<=end){
+        mid=start+(end-start)/2;
+        if(arr[mid]==num){
+            res= mid;
+            end=mid-1;
+        }
+        if(arr[mid]>num)end=mid-1;
+        else start=mid+1;
+    }
+
+    return res;
+}
+
+int infiniteBS(vector<int> &arr,int num){
+    int start=0;
+    int end=1;
+
+    while(arr[end]<num){
+        start=end;
+        end*=2;
+    }
+    // cout<<start<<" "<<end<<endl;
+
+    return BS(arr,start,end,num);
+    
+}
+
+void solve(){
+    int n;
+    cin>>n;
+    int num=1;
+    // cin>>num;
+    vector<int> arr(n);
+    for(auto &it:arr)cin>>it;
+    cout<<infiniteBS(arr,num);
+}
+
+int main(){
+    solve();
+}
